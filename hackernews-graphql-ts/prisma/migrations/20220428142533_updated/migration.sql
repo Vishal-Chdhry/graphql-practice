6d1ff498c390611425bc4cd79735751563cad24b
Vishal Choudhary <contactvishaltech@gmail.com>
2022-04-28 19:57:37 +0530

    adding a db

---
+++ hackernews-graphql-ts/prisma/migrations/20220428142533_updated/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `description` to the `Link` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Link" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "description" TEXT NOT NULL,
+    "url" TEXT NOT NULL
+);
+INSERT INTO "new_Link" ("createdAt", "id", "url") SELECT "createdAt", "id", "url" FROM "Link";
+DROP TABLE "Link";
+ALTER TABLE "new_Link" RENAME TO "Link";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- hackernews-graphql-ts/src/graphql/Link.ts
+++ hackernews-graphql-ts/src/graphql/Link.ts
@@ -1,4 +1,4 @@
-import {extendType, nonNull, objectType, stringArg} from 'nexus';
+import {extendType, idArg, intArg, nonNull, objectType, stringArg} from 'nexus';
 import {NexusGenObjects} from '../../nexus-typegen';
 
 export const Link = objectType({
@@ -35,6 +35,25 @@ export const LinkQuery = extendType({
   },
 });
 
+export const LinkQueryByID = extendType({
+  type: 'Query',
+  definition(t) {
+    t.nonNull.field('getOneLink', {
+      type: 'Link',
+      args: {
+        id: nonNull(intArg()),
+      },
+
+      resolve(parent, args, context, info) {
+        const link = links.find(link => link.id === args.id);
+
+        if (typeof link === 'undefined') throw new Error('Link not found');
+        return link;
+      },
+    });
+  },
+});
+
 export const LinkMutation = extendType({
   type: 'Mutation',
   definition(t) {
@@ -58,6 +77,59 @@ export const LinkMutation = extendType({
         links.push(link);
 
         return link;
+      },
+    });
+  },
+});
+
+export const UpdateLink = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('updateLink', {
+      type: 'Link',
+      args: {
+        id: nonNull(intArg()),
+        url: stringArg(),
+        description: stringArg(),
+      },
+
+      resolve(parent, args, context, info) {
+        const {id, url, description} = args;
+
+        const link_index = links.findIndex(link => link.id === id);
+
+        if (typeof link_index === 'undefined')
+          throw new Error('Link not found');
+
+        links[link_index].url = url ? url : links[link_index].url;
+        links[link_index].description = description
+          ? description
+          : links[link_index].description;
+
+        return links[link_index];
+      },
+    });
+  },
+});
+
+export const deleteLink = extendType({
+  type: 'Mutation',
+  definition(t) {
+    t.nonNull.field('deleteLink', {
+      type: 'Link',
+      args: {
+        id: nonNull(intArg()),
+      },
+
+      resolve(parent, args, context, info) {
+        const {id} = args;
+        const link_index = links.findIndex(link => link.id === id);
+
+        if (typeof link_index === 'undefined')
+          throw new Error('Link not found');
+
+        links = links.filter(link => link.id != link_index);
+        return links[link_index];
       },
     });
   },
